59f92995590eb7fc4b9f98423adef4d461931553
Gilbert <shikweg@gmail.com>
2024-01-17 15:50:32 +0300

    added a fetch to get data from the dictionary API

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+// Event listener for when the DOM content is fully loaded, it calls the `makeLinksSmooth` function
 document.addEventListener("DOMContentLoaded", makeLinksSmooth)
 
+// Function to make navigation links smooth-scroll when clicked
 function makeLinksSmooth() { 
     const navLinks = document.querySelectorAll("nav a"); 
   
@@ -16,5 +18,18 @@ function makeLinksSmooth() {
       targetElement.scrollIntoView({ behavior: "smooth", });
     }
   }
-  
-  
+ 
+// API endpoint for fetching dictionary data
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+const result = document.getElementById("result")
+const btn = document.getElementById("search-btn")
+// Add a click event listener to the search button
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    let input = document.getElementById("search-input").value
+    // Fetch data from the dictionary API based on the user input
+    fetch(`${url}${input}`)
+    .then(res => res.json())
+    .then(data => {console.log(data)
+    });
+})
